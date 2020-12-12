df0c380bdc8cbaf4efe65c1492ce5d05352c25c6
Adi-Deshmukh <68903154+Adi-Deshmukh@users.noreply.github.com>
2020-12-12 13:23:25 +0530

    Add files via upload

---
+++ Moood Muse/Moood-Muse-non-responsive/js/index.js
@@ -0,0 +1,16 @@
+
+/********************
+for the more options  
+ *********************/
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+/************
+ PAGE LOADER
+************/
+
